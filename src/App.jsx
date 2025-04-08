d2e7020d980b7ec89663408c036b2c53d996de60
jeremias <juansebas7011@gmail.com>
2025-04-08 14:17:49 -0500

    contador en react

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+ 
+
+
+
+
+
+function Lista () {
+  const [contador, setContador] = useState(0)
+
+  const incrementar = () => { 
+
+    setContador(contador + 1);
+    console.log (contador);
+  };
+
+  const restar = () => {
+
+    setContador(contador - 1);
+    console.log (contador);
+  };
+
+  const reiniciar = () => {
+    setContador(0);
+  };
+  
+
+  const asignar = () => {
+    setContador("esto es un nuevo elemento ");
+  }
+
+
+  return (
+    <>
+    <div className="Contenedor">
+      <div className="botones"> 
+        <h1 className='numero'>{contador}</h1>
+        <button onClick={incrementar}>Incrementar</button>
+        <button onClick={restar}>Restar</button>
+        <button onClick={reiniciar}>Reiniciar</button>
+      </div>
+      <div className="Asignar">
+      
+        <button onClick={asignar}>Asignar</button>
+      </div>  
+    </div>  
+      
+      
+    </>
+  )
+}
+
+export default Lista
